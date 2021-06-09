1479a2701e532fbad70ba47116a8a6a47a142643
THUNDER MASTER <thndrmaster@gmail.com>
2021-06-09 16:23:46 -0300

    Use e abuse do Django Shell do Python - Aula 029

--- core/views.py
+++ core/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import render
 
 
 def index(request):
+    print(dir(request.user))
+    print(f'User: { request.user }')
+    if str(request.user) == "AnonymousUser":
+        _usuario_logado_ = 'Usuário não Logado'
+    else:
+        _usuario_logado_ = 'Usuário Logado'
     context = {
-        'curso': 'Programação WEB com Django Framework'
+        'curso': 'Programação WEB com Django Framework',
+        'usuario_logado': _usuario_logado_
     }
     return render(request, 'index.html', context)
 

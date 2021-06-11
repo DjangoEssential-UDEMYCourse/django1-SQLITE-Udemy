5d0f4c9faf1435cf429688ee74619459739e8cbe
THUNDER MASTER <thndrmaster@gmail.com>
2021-06-11 11:28:59 -0300

    Apresentando arquivos estaticos css/js/images no Django - Aula 031

--- core/views.py
+++ core/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+
+from django.http import HttpResponse
+from django.template import loader
+
 from .models import Produto
 
 
@@ -20,8 +25,19 @@ def contato(request):
 
 
 def produto(request, pk):
-    _produto_ = Produto.objects.get(id=pk)
+    # _produto_ = Produto.objects.get(id=pk)
+    _produto_ = get_object_or_404(Produto, id=pk)
     context = {
         'produto': _produto_
     }
     return render(request, 'produto.html', context)
+
+
+def error404(request, exception):
+    _template = loader.get_template('status/404.html')
+    return HttpResponse(content=_template.render(), content_type='text/html', charset='utf8', status=404)
+
+
+def error500(request):
+    _template = loader.get_template('status/500.html')
+    return HttpResponse(content=_template.render(), content_type='text/html', charset='utf8', status=500)
